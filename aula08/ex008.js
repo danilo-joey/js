00fd5123dbee0ef4f6b2b52b87bc243b6cbd1ac4
Danilo-Silva <danilo-joey@hotmail.com>
2026-03-23 21:41:30 -0300

    jsex

---
+++ aula08/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.1
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if (vel > 60) {
+    console.log(`Voce foi notificado por excesso de Velocidade`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`) 
---
+++ aula09/ex013.js
@@ -0,0 +1,31 @@
+var agora = new Date()
+var hoje = agora.getDay()
+console.log('O dia de hoje é')
+
+hoje=7
+
+switch(hoje) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda-feira')
+        break
+    case 2:
+        console.log('Terca-feira')
+        break
+    case 3:
+        console.log('Quarta-feira')
+        break
+    case 4:
+        console.log('Quinta-feira')
+        break
+    case 5:
+        console.log('Sexta-feira')
+        break
+    case 6:
+        console.log('Sabado')
+    
+    default:
+        console.log('[ERRO] Dia invalido.')
+}
